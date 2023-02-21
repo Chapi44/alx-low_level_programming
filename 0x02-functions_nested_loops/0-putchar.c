de729c12f369399cc3d7c5208311f5b9b36cc8b9
chapi44 <enterct35i@gmail.com>
2023-02-21 06:51:44 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * main - prints _putchar and new line
+ * Return: Always 0
+ */
+int main(void)
+	 _pitchar('_');
+	 _pitchar('p');
+	 _pitchar('u');
+	 _pitchar('t');
+	 _pitchar('c');
+	 _pitchar('h');
+	 _pitchar('a');
+	 _pitchar('r');
+	 _pitchar('\n');
+
+	 return (0);
+
