404db6b4b3ef1aba94eff0d4fb20d162b8c88913
chapi44 <enterct35i@gmail.com>
2023-03-20 10:04:02 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,17 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ *
+ * Desc: This header file defines a macro named SIZE an
+ * abbreviation for token 1024.
+ *
+ * Author: Gizachew Bayness (eleccrazy).
+ *
+ * Date Created: May 8, 2022
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,17 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * File: 1-pi.h
+ *
+ * Desc: This header file defines a macro named PI as an
+ * abbreviation for the token 3.14159265359.
+ *
+ * Author: Gizachew Bayness (eleccrazy)
+ *
+ * Date Created: May 8, 2022
+ */
+
+#define PI 3.14159265359
+
+#endif
